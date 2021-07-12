44f20302d31a67070099f6a9c689040e55176e68
punit <punitparmar944@gmail.com>
2021-07-12 19:19:38 +0530

    remove php pro

--- php/forum Project/partials/_logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-echo "<h2>Loggin you out. Please wait....</h2>";
-session_destroy();
-header("Location: ../index.php");
-?>
--- php/forum Project/partials/foter.php
+++
@@ -1,3 +0,0 @@
-<div class="container-fiuid bg-dark text-light mt-3">
-    <p class="text-center py-3 mb-0">copyright iDisucss Coding Form 2021 | All rights </p>
-</div>
